20b121e1a824b6e0e9187121e1021f14efe85a70
Matheus Padilha Ansolin <matheusp.ansolin@gmail.com>
2023-10-29 10:35:06 -0300

    complete API

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,15 +1,33 @@
 package com.example.cardapio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
-    @GetMapping
-    public void getAll(){
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
     }
+    @Autowired
+    private FoodRepository repository;
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = (List<FoodResponseDTO>) repository.findAll().stream().map(FoodResponseDTO::new);
+        return foodList;
+    }
+
 }
 
